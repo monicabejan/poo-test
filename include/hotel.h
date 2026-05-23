c485546b5ad61fedc6f8f9b4bb841d7dd0137922
monicabejan <bejanmonica2006@gmail.com>
2026-05-23 15:13:20 +0300

    Add hotel class header for Singleton pattern

---
+++ include/hotel.h
@@ -0,0 +1,15 @@
+#ifndef HOTEL_H
+#define HOTEL_H
+
+#include <iostream>
+
+class hotel{
+  hotel () {}
+  hotel(const hotel &) = delete;
+  hotel& operator=(const hotel&)=delete; // prevenim copierea (Singleton)
+public:
+  static hotel* getInstanta();
+
+};
+
+#endif
