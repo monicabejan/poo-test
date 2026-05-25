079ed82576ddfff672c790d03e48fd37156040a7
monicabejan <bejanmonica2006@gmail.com>
2026-05-25 11:40:07 +0000

    fix bugs

--- include/persoana.h
+++ include/persoana.h
@@ -18,8 +18,8 @@ public:
     void doSomething () ;
     virtual ~persoana()=default;
 
-    std::string& getNume() const {return nume;}
-    std::string& getPrenume() const {return prenume;}
+    const std::string& getNume() const {return nume;}
+    const std::string& getPrenume() const {return prenume;}
 };
 
 
@@ -30,7 +30,7 @@ public:
     ~client() override = default;
     client(const std::string& nume, const std::string& prenume, const std::string& abonament): persoana(nume, prenume), tipAbonament(abonament) {}
 
-    std::string& getTipAbonament() const {return tipAbonament;}
+    const std::string& getTipAbonament() const {return tipAbonament;}
     void upgradeAbonament(const std::string& abNou) { tipAbonament = abNou; }
 
 };
