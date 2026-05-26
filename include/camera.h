8e4263e6cada51e265a580d22f46fe8f3eb99d38
monicabejan <bejanmonica2006@gmail.com>
2026-05-26 12:25:36 +0000

    cc si op= in camera

--- include/camera.h
+++ include/camera.h
@@ -8,26 +8,31 @@ protected:
     int nrCamera;
     int etaj;
     double pret;
-    bool status; // true daca camera e ocupata
+    bool status; 
 public:
-    camera(int nr, int et, double p, bool status = false) : nrCamera(nr), etaj(et), pret(p), status(status) {}
+    camera(int nr, int et, double p, bool status = false);
+    camera(const camera& other);
+    camera& operator=(const camera& other);
+    virtual ~camera()=default;
+
     int esteOcupata() const {return status;}
     int getNrCamera() const {return nrCamera;}
     void setStatus(bool st){status=st;}
     double getPret() const { return pret;}
-
     virtual std::string getTip() const =0;
-    friend std::ostream& operator<<(std::ostream& os, const camera& c); 
-    virtual ~camera()=default;
-
     virtual void afisareDetalii (std::ostream& os) const =0;
+
+    friend std::ostream& operator<<(std::ostream& os, const camera& c); 
 };
 
 
 
 class cameraSingle : public camera{
 public:
-    cameraSingle(int nr, int et, double p=150.0):camera(nr, et, p) {}
+    cameraSingle(int nr, int et, double p=150.0);
+    cameraSingle(const cameraSingle& other);
+    cameraSingle& operator=(const cameraSingle& other);
+   
     std::string getTip() const override { return "Single";}
     void afisareDetalii(std::ostream& os) const override;
 };
@@ -37,7 +42,10 @@ public:
 class cameraDouble : public camera{
     std::string tipConfiguratie;
 public:
-    cameraDouble(int nr, int et, const std::string& config, double p=250.0) : camera(nr, et, p), tipConfiguratie(config) {}
+    cameraDouble(int nr, int et, const std::string& config, double p=250.0) ;
+    cameraDouble(const cameraDouble& other);
+    cameraDouble& operator=(const cameraDouble& other);
+    
     std::string getTip() const override { return "Double";}
     const std::string& getConfiguratie() const {return tipConfiguratie;}
     void afisareDetalii(std::ostream& os) const override;
@@ -46,7 +54,10 @@ public:
 class penthouse: public camera {
     int nrDormitoare;
 public:
-    penthouse(int nr, int et, int nrDorm, double p=500.0): camera(nr, et, p), nrDormitoare(nrDorm) {}
+    penthouse(int nr, int et, int nrDorm, double p=500.0);
+    penthouse(const penthouse& other);
+    penthouse& operator=(const penthouse& other);
+    
     std::string getTip() const override {return "Penthouse";}
     int getDormitoare() const {return nrDormitoare;}
     void afisareDetalii(std::ostream& os) const override;
--- src/camera.cpp
+++ src/camera.cpp
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
+camera::camera(int nr, int et, double p, bool st) : nrCamera(nr), etaj(et), pret(p), status(st) {}
+camera::camera(const camera& other) : nrCamera(other.nrCamera), etaj(other.etaj), pret(other.pret), status(other.status) {}
+camera& camera::operator=(const camera& other) {
+    if(this!=&other){
+        nrCamera=other.nrCamera;
+        etaj=other.etaj;
+        pret=other.pret;
+        status=other.status;
+    }
+    return *this;
+}
 std::ostream& operator<<(std::ostream& os, const camera& c) {
     os<<"\nNr: "<<c.nrCamera
       <<"\nEtaj: "<<c.etaj
@@ -9,6 +20,35 @@ std::ostream& operator<<(std::ostream& os, const camera& c) {
     c.afisareDetalii(os);
     return os;
 } 
+
+cameraSingle::cameraSingle(int nr, int et, double p):camera(nr, et, p) {}
+cameraSingle::cameraSingle(const cameraSingle& other): camera(other) {}
+cameraSingle& cameraSingle::operator=(const cameraSingle& other){
+    if(this!=&other)
+        camera::operator=(other);
+    return *this;
+}
+
+cameraDouble::cameraDouble(int nr, int et, const std::string& config, double p) : camera(nr, et, p), tipConfiguratie(config) {}
+cameraDouble::cameraDouble(const cameraDouble& other) : camera(other), tipConfiguratie(other.tipConfiguratie) {}
+cameraDouble& cameraDouble::operator=(const cameraDouble& other){
+    if(this!=&other){
+        camera::operator=(other);
+        tipConfiguratie=other.tipConfiguratie;
+    }
+    return *this;
+}
+
+
+penthouse::penthouse(int nr, int et, int nrDorm, double p): camera(nr, et, p), nrDormitoare(nrDorm) {}
+penthouse::penthouse(const penthouse& other): camera(other), nrDormitoare(other.nrDormitoare) {}
+penthouse& penthouse::operator=(const penthouse& other){
+    if(this!=&other){
+        camera::operator=(other);
+        nrDormitoare=other.nrDormitoare;
+    }
+    return *this;
+}
 
 void cameraSingle::afisareDetalii(std::ostream& os) const {
     os<<" ";
